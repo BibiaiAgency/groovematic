55a4ff3e518a2218ba7f23648e08c7491a7d388e
Bibiai <pedro@bibiai.es>
2023-11-04 07:06:21 +0100

    Game added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
  // Cargar header
- fetch('/parts/header')
+ fetch('/parts/header.html')
  .then(response => response.text())
  .then(data => {
    document.querySelector('#header').innerHTML = data;
 });
 
  // Cargar footer
- fetch('/parts/footer')
+ fetch('/parts/footer.html')
  .then(response => response.text())
  .then(data => {
    document.querySelector('#footer').innerHTML = data;
